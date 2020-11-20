fdccdb96a252dfdc9907cb5191970bbc3da433f0
auk6251 <auk6251@naver.com>
2020-11-20 14:53:37 +0900

    db.sql 정리해서 다시 저장

--- db.sql
+++ db.sql
@@ -1,7 +1,7 @@
 # 데이터 베이스 생성
-DROP DATABASE IF EXISTS lolHi;
-CREATE DATABASE lolHi;
-USE lolHi;
+DROP DATABASE IF EXISTS lolHiT;
+CREATE DATABASE lolHiT;
+USE lolHiT;
 # 게시물 테이블 생성
 CREATE TABLE article (
     id INT(10) UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
@@ -30,7 +30,6 @@ CREATE TABLE `member` (
     loginPw CHAR(100) NOT NULL,
     `name` CHAR(100) NOT NULL
 );
-
 # 회원 생성
 INSERT INTO `member`
 SET regDate = NOW(),
@@ -38,23 +37,30 @@ updateDate = NOW(),
 loginId = 'test1',
 loginPw = 'test1',
 `name` = '홍길동';
-
 INSERT INTO `member`
 SET regDate = NOW(),
 updateDate = NOW(),
 loginId = 'test2',
 loginPw = 'test2',
 `name` = '홍길순';
-
 # 게시물 테이블에 memberId 칼럼 추가
 ALTER TABLE article ADD COLUMN memberId INT(10) UNSIGNED NOT NULL AFTER updateDate;
 # 기존 게시물들의 작성자는 1번 회원으로 정한다.
 UPDATE article SET memberId = 1 WHERE memberId = 0;
-
-SELECT * FROM article; 
-
-
-
+# 게시물 데이터 추가
+INSERT INTO article
+SET regDate = NOW(),
+updateDate = NOW(),
+title = '제목3',
+`body` = '내용3',
+memberId = 2;
+INSERT INTO article
+SET regDate = NOW(),
+updateDate = NOW(),
+title = '제목4',
+`body` = '내용4',
+memberId = 2;
+# 댓글 테이블 생성
 CREATE TABLE reply (
     id INT(10) UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
     regDate DATETIME NOT NULL,
@@ -64,5 +70,60 @@ CREATE TABLE reply (
     relId INT(10) UNSIGNED NOT NULL, # 관련 데이터 ID
     relTypeCode CHAR(50) NOT NULL # 관련 데이터 타입
 );
+# 댓글 데이터 추가
+INSERT INTO reply
+SET regDate = NOW(),
+updateDate = NOW(),
+relTypeCode = 'article',
+relId = 1,
+`body` = '댓글 1',
+memberId = 1;
+INSERT INTO reply
+SET regDate = NOW(),
+updateDate = NOW(),
+relTypeCode = 'article',
+relId = 1,
+`body` = '댓글 2',
+memberId = 1;
+INSERT INTO reply
+SET regDate = NOW(),
+updateDate = NOW(),
+relTypeCode = 'article',
+relId = 1,
+`body` = '댓글 3',
+memberId = 1;
+INSERT INTO reply
+SET regDate = NOW(),
+updateDate = NOW(),
+relTypeCode = 'article',
+relId = 1,
+`body` = '댓글 4',
+memberId = 2;
+# 게시판 테이블 추가
+CREATE TABLE board (
+    id INT(10) UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    regDate DATETIME NOT NULL,
+    updateDate DATETIME NOT NULL,
+    `name` CHAR(20) UNIQUE NOT NULL,
+    `code` CHAR(20) UNIQUE NOT NULL
+);
+INSERT INTO board
+SET regDate = NOW(),
+updateDate = NOW(),
+`name` = '공지사항',
+`code` = 'notice';
+INSERT INTO board
+SET regDate = NOW(),
+updateDate = NOW(),
+`name` = '자유',
+`code` = 'free';
+# 게시물 테이블에 boardId 칼럼 추가
+ALTER TABLE article ADD COLUMN boardId INT(10) UNSIGNED NOT NULL AFTER updateDate;
+UPDATE article SET boardId = 1 WHERE id <= 2;
+UPDATE article SET boardId = 2 WHERE id > 2;
 
-SELECT * FROM reply; 
+
+
+# 회원테이블에 email 칼럼 추가
+ALTER TABLE MEMBER ADD COLUMN email CHAR(100) NOT NULL NOT NULL AFTER `name`;
+UPDATE `member` SET email = 'jangka512@gmail.com' WHERE email = '';
